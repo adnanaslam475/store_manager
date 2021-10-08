315a0c25abc8e07dfde3240bc7a2b46f33ca4ea9
adnan aslam <adnanaslam475@gmail.com>
2021-10-09 00:52:01 +0500

    firstsss

---
+++ src/configs/constant.js
@@ -0,0 +1,8 @@
+export const API = 'https://cellar97.com';
+
+export const CONSUMER_KEY = 'ck_1541d1af4c057242988b52375052c4c7acbd4447';
+export const CONSUMER_SECRET = 'cs_cac41359e8014ac1fcddd46ac9da84c3822db85a';
+
+export const ONE_SIGNAL_APP_ID = 'f86b1047-7111-4f28-bfdd-f7729c7e1a1b';
+
+export const PLUGIN = 'app-builder';
